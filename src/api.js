854020e3dc74f1b84641b8f478d22610de270f1f
Daphne <daphne.westerdijk@hotmail.com>
2023-05-09 13:54:54 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const geoAPIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e96f6ea17dmsh463f02e8f30733dp1ab50djsn77a071fd71bd',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '568c88924cce26caa58e4e7458e804af';
+
---
+++ src/components/title/title.js
@@ -0,0 +1,11 @@
+import './title.css';
+
+const Title = () => {
+    return (
+        <div className='container-title'>
+            <div className='title-app'>Weather App by Daphne</div>
+        </div>
+    )
+}
+
+export default Title;
